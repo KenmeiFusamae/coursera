e6090f8a74c2aa55c39156f6022d589be8666a09
KenmeiFusamae <zgmfx2oaf91@gmail.com>
2019-07-01 01:06:11 +0900

    week4 test

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -35,10 +35,20 @@ grad = zeros(size(theta));
 %           temp(1) = 0;   % because we don't add anything for j = 0  
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
+h = sigmoid(X * theta);
+
+theta(1,1) = 0;
+
+Jreg = lambda / (2 * m) *(theta'*theta);
+J = 1/m * ((-y)' * log(h) - (1-y)'*log(1-h) )+ Jreg;
 
 
-
-
+%temp = theta; 
+%temp(1) = 0;   % because we don't add anything for j = 0  
+%gradreg = lambda / m * temp ; %grad j= 0
+%grad = gradreg + 1/m * X' *(h-y);
+grad = (1/m) * X' * (h - y) + (lambda/m) * theta;
+grad(1,1) = (1/m) * X'(1,:) * (h - y);
 
 
 
